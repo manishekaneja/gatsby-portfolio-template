34f0e94dfc81d4da0c18bf4ae4ed28e648e73bcf
Manish Aneja <manishekaneja@gmail.com>
2021-02-17 01:02:24 +0530

    Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,25 @@ module.exports = {
     author: `@manishekaneja`,
   },
   plugins: [
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //     trackingIds: [
+    //       "GA-TRACKING_ID", // Google Analytics / GA
+    //       "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+    //       "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+    //     ],
+    //   },
+    // },
+    {
+      resolve:"gatsby-plugin-google-analytics",
+      options:{
+        trackingId:"G-GZ2HZ9G4Y4",
+        head:true,
+        anonymize:true
+      }
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
